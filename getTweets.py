7a90c8c981025cde34c7bbad580354e3bb295bfb
mlum123 <michellelum123@gmail.com>
2022-11-04 16:36:12 -0700

    Update getTweets script to properly get 10 years of data, cashtags, and entity probabilities, using get_tweets and get_users

--- getTweets.py
+++ getTweets.py
@@ -19,8 +19,8 @@ import time
 
 FIELDS = ["created_at", "text", "public_metrics", "context_annotations", "entities", "referenced_tweets", "author_id"]
 client = tweepy.Client(bearer_token=TWITTER_API_BEARER)
-API_CALLS = {"get_user": 0, "get_users_tweets": 0, "get_tweet": 0}
-RATE_LIMITS = {"get_user": 900, "get_users_tweets": 900, "get_tweet": 300}
+API_CALLS = {"get_user": 0, "get_users": 0, "get_users_tweets": 0, "get_tweets": 0}
+RATE_LIMITS = {"get_user": 300, "get_users": 300, "get_users_tweets": 900, "get_tweets": 300}
 
 COLUMNS = ["username", "is_quoted_tweet", "is_retweeted_tweet", "tweet_id", "created_at", "text", "hashtags", "like_count", "reply_count", "retweet_count", "referenced_tweets", "context_annotations", "entity_annotations"]
 TWEET_COUNT = 100
@@ -36,7 +36,7 @@ def check_rate_limit(api_call: str):
         API_CALLS["get_tweet"] = 0
         print(API_CALLS)
 
-def get_tweets_for_user(username: str, get_quoted_tweets: bool, get_retweeted_tweets: bool):
+def get_tweets_for_user(username: str, num_years: int, get_quoted_tweets: bool, get_retweeted_tweets: bool):
     user = client.get_user(username=username)
     API_CALLS["get_user"] += 1
     print(API_CALLS)
@@ -45,7 +45,7 @@ def get_tweets_for_user(username: str, get_quoted_tweets: bool, get_retweeted_tw
     end_time = datetime.datetime.now(datetime.timezone.utc).replace(microsecond=0)
     one_year_delta = datetime.timedelta(days=365)
     end_time_formatted = str(end_time.isoformat())
-    start_time_formatted = str((end_time - one_year_delta).isoformat())
+    start_time_formatted = str((end_time - num_years * one_year_delta).isoformat())
     
     rows = []
     quoted_tweet_ids = set()
@@ -55,21 +55,21 @@ def get_tweets_for_user(username: str, get_quoted_tweets: bool, get_retweeted_tw
     while True:
         print(f"{end_time_formatted = }")
         print(f"{start_time_formatted = }")
-        tweets = client.get_users_tweets(id=user.data.id, tweet_fields=FIELDS, end_time=end_time_formatted, start_time=start_time_formatted, max_results=TWEET_COUNT, exclude=['replies'])
+        tweets = client.get_users_tweets(id=user.data.id, tweet_fields=FIELDS, end_time=end_time_formatted, start_time=start_time_formatted, max_results=TWEET_COUNT)
         API_CALLS["get_users_tweets"] += 1
         print(API_CALLS)
         check_rate_limit("get_users_tweets")
 
         if not tweets.data:
             break
-
-        print(f"Got {len(tweets.data)} tweets for {username}.")
             
+        print(f"Got {len(tweets.data)} tweets for {username}.")
+        
         for tweet in tweets.data:
             tweet_type = "regular" # assume regular tweet
-            parsed_tweet = parse_tweet_data(username, tweet)
+            parsed_tweet = parse_tweet_data(tweet, username = username)
             if parsed_tweet:
-                referenced_tweets = parsed_tweet[10]
+                referenced_tweets = parsed_tweet[11] # referenced_tweets are at index 11
                 if referenced_tweets:
                     for reference in referenced_tweets:
                         tweet_id, tweet_type = reference
@@ -80,36 +80,87 @@ def get_tweets_for_user(username: str, get_quoted_tweets: bool, get_retweeted_tw
                 
                 if tweet_type != "retweeted" and tweet_type != "replied_to":
                     rows.append(parsed_tweet)
-
+                
                 # Update end_time to created_at time of last tweet
                 last_tweet_time = parsed_tweet[4]   # created_at is at index 4
                 end_time_formatted = "T".join(last_tweet_time.split())
-                
-        if len(tweets.data) < 100:
-            # No more tweets btwn start_time and original end_time
-            break
+            
+    author_id_to_username_map = {}
     
     # Get quoted tweets
     if get_quoted_tweets and len(quoted_tweet_ids) > 0:
-        print(f"Getting quoted tweets for {username}.")
-        for tweet_id in quoted_tweet_ids:
-            tweet = client.get_tweet(id=tweet_id, tweet_fields=FIELDS)
-            API_CALLS["get_tweet"] += 1
+        print(f"Getting {len(quoted_tweet_ids)} quoted tweets for {username}.")
+        quoted_tweet_ids = list(quoted_tweet_ids)
+        
+        tweets = []
+        for i in range(0, len(quoted_tweet_ids), 100): # need for loop since get_tweets can only get up to 100 tweets at a time
+            tweets += client.get_tweets(quoted_tweet_ids[i:i+100], tweet_fields=FIELDS).data
+            API_CALLS["get_tweets"] += 1
             print(API_CALLS)
-            check_rate_limit("get_tweet")
-            parsed_tweet = parse_tweet_data(None, tweet.data, is_quoted_tweet = True)
+            check_rate_limit("get_tweets")
+        
+        print(f"Got {len(tweets)} quoted tweets for {username}.")
+        
+        # Get usernames for authors of quoted tweets
+        print(f"Getting usernames of quoted tweets for {username}.")
+        author_ids = set()
+        for tweet in tweets:
+            author_ids.add(tweet["author_id"])
+        author_ids = list(author_ids)
+        
+        users = []
+        for i in range(0, len(author_ids), 100): # need for loop since get_users can only get up to 100 users at a time
+            users += client.get_users(ids=author_ids[i:i+100]).data
+            API_CALLS["get_users"] += 1
+            print(API_CALLS)
+            check_rate_limit("get_users")
+        
+        print(f"Got {len(users)} usernames of quoted tweets for {username}.")
+
+        for user in users:
+            author_id_to_username_map[user.id] = user.username        
+        
+        # Add quoted tweets to df
+        for tweet in tweets:
+            parsed_tweet = parse_tweet_data(tweet, author_id_to_username_map = author_id_to_username_map, is_quoted_tweet = True)
             if parsed_tweet:
                 rows.append(parsed_tweet)
-    
+
     # Get retweeted tweets
     if get_retweeted_tweets and len(retweeted_tweet_ids) > 0:
-        print(f"Getting retweeted tweets for {username}.")
-        for tweet_id in retweeted_tweet_ids:
-            tweet = client.get_tweet(id=tweet_id, tweet_fields=FIELDS)
-            API_CALLS["get_tweet"] += 1
+        print(f"Getting {len(retweeted_tweet_ids)} retweeted tweets for {username}.")
+        retweeted_tweet_ids = list(retweeted_tweet_ids)
+        tweets = []
+        for i in range(0, len(retweeted_tweet_ids), 100): # need for loop since get_tweets can only get up to 100 tweets at a time
+            tweets += client.get_tweets(retweeted_tweet_ids[i:i+100], tweet_fields=FIELDS).data
+            API_CALLS["get_tweets"] += 1
             print(API_CALLS)
-            check_rate_limit("get_tweet")
-            parsed_tweet = parse_tweet_data(None, tweet.data, is_retweeted_tweet = True)
+            check_rate_limit("get_tweets")
+
+        print(f"Got {len(tweets)} retweeted tweets for {username}.")
+        
+        # Get usernames for authors of retweeted tweets
+        print(f"Getting usernames of retweeted tweets for {username}.")
+        author_ids = set()
+        for tweet in tweets:
+            author_ids.add(tweet["author_id"])
+        author_ids = list(author_ids)
+        
+        users = []
+        for i in range(0, len(author_ids), 100): # need for loop since get_users can only get up to 100 users at a time
+            users += client.get_users(ids=author_ids[i:i+100]).data
+            API_CALLS["get_users"] += 1
+            print(API_CALLS)
+            check_rate_limit("get_users")
+            
+        print(f"Got {len(users)} usernames of retweeted tweets for {username}.")
+
+        for user in users:
+            author_id_to_username_map[user.id] = user.username      
+            
+        # Add retweeted tweets to df
+        for tweet in tweets:
+            parsed_tweet = parse_tweet_data(tweet, author_id_to_username_map = author_id_to_username_map, is_retweeted_tweet = True)
             if parsed_tweet:
                 rows.append(parsed_tweet)
         
@@ -117,27 +168,23 @@ def get_tweets_for_user(username: str, get_quoted_tweets: bool, get_retweeted_tw
     
     return df
 
-def parse_tweet_data(username, tweet, is_quoted_tweet = False, is_retweeted_tweet = False):
-    if not username:
-        user = client.get_user(id=tweet.author_id)
-        API_CALLS["get_user"] += 1
-        print(API_CALLS)
-        check_rate_limit("get_user")
-        username = user.data.username
-    tweet_id = tweet.id
-    created_at = str(tweet.created_at)
-    text = tweet.text
+def parse_tweet_data(tweet, username = None, author_id_to_username_map = None, is_quoted_tweet = False, is_retweeted_tweet = False):
+    if not username and author_id_to_username_map:
+        username = author_id_to_username_map[tweet["author_id"]]
+    tweet_id = tweet["id"]
+    created_at = str(tweet["created_at"])
+    text = tweet["text"]
+    cashtags = parse_entity_cashtags(tweet)
     hashtags = parse_entity_hashtags(tweet)
-    metrics = tweet.public_metrics
+    metrics = tweet["public_metrics"]
     like_count = metrics["like_count"]
     reply_count = metrics["reply_count"]
     retweet_count = metrics["retweet_count"]
-    is_reply = False
     referenced_tweets = parse_referenced_tweets(tweet)
     context_annotations = parse_context_annotations(tweet)
     entities = parse_entity_annotations(tweet)
     
-    return [username, is_quoted_tweet, is_retweeted_tweet, tweet_id, created_at, text, hashtags, like_count, reply_count, retweet_count, referenced_tweets, context_annotations, entities]
+    return [username, is_quoted_tweet, is_retweeted_tweet, tweet_id, created_at, text, cashtags, hashtags, like_count, reply_count, retweet_count, referenced_tweets, context_annotations, entities]
 
 def parse_referenced_tweets(tweet):
     referenced_tweets = set()
@@ -145,8 +192,8 @@ def parse_referenced_tweets(tweet):
         return None
     
     for obj in tweet.referenced_tweets:
-        tweet_id = obj['id']
-        tweet_type = obj['type']
+        tweet_id = obj["id"]
+        tweet_type = obj["type"]
         
         referenced_tweets.add((tweet_id, tweet_type))
 
@@ -157,30 +204,41 @@ def parse_entity_annotations(tweet):
     if not tweet.entities or "annotations" not in tweet.entities:
         return None
     
-    for obj in tweet.entities['annotations']:
-        annotation_type = obj['type']
-        annotation_text = obj['normalized_text']
+    for obj in tweet.entities["annotations"]:
+        annotation_probability = obj["probability"]
+        annotation_type = obj["type"]
+        annotation_text = obj["normalized_text"]
         
-        info_tuples.add((annotation_type, annotation_text))
+        info_tuples.add((annotation_probability, annotation_type, annotation_text))
 
     return info_tuples
+
+def parse_entity_cashtags(tweet):
+    cashtags = set()
+    if "entities" not in tweet or "cashtags" not in tweet["entities"]:
+        return None
+    
+    for obj in tweet["entities"]["cashtags"]:
+        cashtags.add(obj["tag"])
+        
+    return cashtags
 
 def parse_entity_hashtags(tweet):
     hashtags = set()
     if not tweet.entities or "hashtags" not in tweet.entities:
         return None
     
-    for obj in tweet.entities['hashtags']:
-        hashtags.add(obj['tag'])
+    for obj in tweet.entities["hashtags"]:
+        hashtags.add(obj["tag"])
         
     return hashtags
 
 def parse_context_annotations(tweet):
     info_tuples = set()
     for obj in tweet.context_annotations:
-        domain_id = obj['domain']['id']
-        domain_name = obj['domain']['name']
-        entity_name = obj['entity']['name']
+        domain_id = obj["domain"]["id"]
+        domain_name = obj["domain"]["name"]
+        entity_name = obj["entity"]["name"]
         
         info_tuples.add((domain_id, domain_name, entity_name))
     
@@ -193,7 +251,7 @@ def parse_context_annotations(tweet):
 # ipython
 # run getTweets.py sp_500_twitter_subsidiaries_manual_no_duplicates.csv
 if __name__ == "__main__":
-    output_folder =  'data/tweets/one_year_no_quoted_no_retweeted/'
+    output_folder =  'data/tweets/ten_years/'
 
     # Read CSV of Twitter handles
     twitter_handle_csv = sys.argv[1]
@@ -201,6 +259,6 @@ if __name__ == "__main__":
     twitter_handles = twitter_handle_df["Twitter Handle"].dropna()  # Drop nulls (some companies don't have Twitters)
     for handle in twitter_handles:
         print(f"Getting tweets for {handle}.")
-        company_df = get_tweets_for_user(handle, False, False) # Last 2 params mean without quoted tweets and without retweeted tweets
-        company_df.drop_duplicates(subset=['tweet_id'])        # In case companies retweet or quote tweet themselves
+        company_df = get_tweets_for_user(handle, 10, True, True) # Last 2 params mean with quoted tweets and retweeted tweets
+        company_df.drop_duplicates(subset=['tweet_id'])          # In case companies retweet or quote tweet themselves
         company_df.to_csv(f"{output_folder}{handle}_tweets.csv")
